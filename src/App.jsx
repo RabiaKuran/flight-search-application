2e8bdc4a9fb30cce4fec9eafbbf3ae50eb15a032
rabiakuran <59939284+RabiaKuran@users.noreply.github.com>
2023-08-17 15:48:24 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Navbar/>
       <p>hello</p>
-    </div>
+      <Home></Home>
+      
+    </>
   );
 }
 
